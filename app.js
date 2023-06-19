7d5853986668a4ade5395c549bcab4d3a8667c8f
Josh Sellers <joshuasellersd@gmail.com>
2023-06-18 18:05:49 -0700

    more notes from video

--- app.js
+++ app.js
@@ -7,3 +7,10 @@ function sayHello(name){
 // global = window in node 
 // in JS vars are added to windo
 //  in node, vars are not added to global
+
+// in JS this can be bad if we have duplicate 
+// functions that override eachother due to global scope
+// modules for node fix this - separation and encapsulation
+
+// files will have a main module that makes things private
+// console.log(module)

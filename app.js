ea605618241055d0a0aefa3a0db0959389c06ea4
Josh Sellers <joshuasellersd@gmail.com>
2023-06-18 18:01:45 -0700

    more notes

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ function sayHello(name){
     console.log('Hello' + name);
 }
 
-sayHello("Josh");
+// sayHello("Josh");
+
+// global = window in node 
+// in JS vars are added to windo
+//  in node, vars are not added to global
